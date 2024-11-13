256f53facb1315a0aabe1b0d478c2c8796be53ae
Matheus Rocco <matheushrocco@gmail.com>
2024-11-12 20:21:20 -0500

    feat: generate report excel file

--- src/CashFlow.Domain/Repositories/Expenses/IExpensesReadOnlyRepository.cs
+++ src/CashFlow.Domain/Repositories/Expenses/IExpensesReadOnlyRepository.cs
@@ -4,5 +4,6 @@ namespace CashFlow.Domain.Repositories.Expenses;
 public interface IExpensesReadOnlyRepository
 {
     Task<List<Expense>> GetAll();
-    Task<Expense?> GetById(long id); 
+    Task<Expense?> GetById(long id);
+    Task<List<Expense>> FilterByMonth(DateOnly date);
 }
