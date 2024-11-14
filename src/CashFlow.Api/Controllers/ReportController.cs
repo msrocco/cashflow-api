0f2d8d2cae88606bf92acc9a943d3b8bbcc7e25a
Matheus Rocco <matheushrocco@gmail.com>
2024-11-13 22:24:59 -0500

    feat: generate expense report pdf

--- src/CashFlow.Api/Controllers/ReportController.cs
+++ src/CashFlow.Api/Controllers/ReportController.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Reports.Excel;
+using CashFlow.Application.UseCases.Expenses.Reports.Pdf;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
 
@@ -21,6 +22,19 @@ public class ReportController : ControllerBase
 
         return NoContent();
     }
+    
+    [HttpGet("pdf")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetPdf(
+        [FromServices] IGenerateExpensesReportPdfUseCase useCase, 
+        [FromQuery] DateOnly month)
+    {
+        byte[] file = await useCase.Execute(month);
 
+        if (file.Length > 0)
+            return File(file, MediaTypeNames.Application.Pdf, "report.pdf");
 
+        return NoContent();
+    }
 }
