da34016cbd39a434b467b0449a9cca781932723e
Matheus Rocco <matheushrocco@gmail.com>
2024-11-17 18:15:42 -0500

    feat: update user

--- src/CashFlow.Infrastructure/DataAccess/Repositories/UserRepository.cs
+++ src/CashFlow.Infrastructure/DataAccess/Repositories/UserRepository.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CashFlow.Infrastructure.DataAccess.Repositories;
 
-internal class UserRepository : IUserReadOnlyRepository, IUserWriteOnlyRepository
+internal class UserRepository : IUserReadOnlyRepository, IUserWriteOnlyRepository, IUserUpdateOnlyRepository
 {
     private readonly CashFlowDbContext _dbContext;
 
@@ -20,8 +20,18 @@ internal class UserRepository : IUserReadOnlyRepository, IUserWriteOnlyRepositor
         return await _dbContext.Users.AnyAsync(user => user.Email.Equals(email));
     }
 
+    public async Task<User> GetById(long id)
+    {
+        return await _dbContext.Users.FirstAsync(user => user.Id == id);
+    }
+
     public async Task<User?> GetUserByEmail(string email)
     {
         return await _dbContext.Users.AsNoTracking().FirstOrDefaultAsync(user => user.Email.Equals(email));
+    }
+
+    public void Update(User user)
+    {
+        _dbContext.Users.Update(user);
     }
 }
