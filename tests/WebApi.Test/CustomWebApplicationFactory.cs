8cd3ab64e5f3de53197d1d2a6e2559e20c3b712a
Matheus Rocco <matheushrocco@gmail.com>
2024-11-17 11:18:58 -0500

    test: create custom class fixture

--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Domain.Security.Tokens;
 using CashFlow.Infrastructure.DataAccess;
 using CommonTestUtilities.Entities;
 using Microsoft.AspNetCore.Hosting;
@@ -12,6 +13,7 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
     private CashFlow.Domain.Entities.User _user;
     private string _password;
+    private string _token;
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
@@ -29,8 +31,11 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
                 var scope = services.BuildServiceProvider().CreateScope();
                 var dbContext = scope.ServiceProvider.GetRequiredService<CashFlowDbContext>();
                 var passwordEncripter = scope.ServiceProvider.GetRequiredService<IPasswordEncripter>();
+                var tokenGenerator = scope.ServiceProvider.GetRequiredService<IAccessTokenGenerator>();
 
                 StartDatabase(dbContext, passwordEncripter);
+
+                _token = tokenGenerator.Generate(_user);
             });
     }
 
@@ -39,6 +44,8 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
     public string GetName() => _user.Name;
 
     public string GetPassword() => _password;
+
+    public string GetToken() => _token;
 
     private void StartDatabase(CashFlowDbContext dbContext, IPasswordEncripter passwordEncripter)
     {
---
+++ tests/WebApi.Test/Expenses/Register/RegisterExpenseTest.cs
@@ -0,0 +1,61 @@
+﻿using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using System.Globalization;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using WebApi.Test.InlineData;
+
+namespace WebApi.Test.Expenses.Register;
+
+public class RegisterExpenseTest : CashFlowClassFixture
+{
+    private const string METHOD = "api/expenses";
+
+    private readonly string _token;
+
+    public RegisterExpenseTest(CustomWebApplicationFactory webApplicationFactory) : base(webApplicationFactory)
+    {
+        _token = webApplicationFactory.GetToken();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+
+        var result = await DoPost(requestUri: METHOD, request: request, token: _token);
+
+        result.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+
+        response.RootElement.GetProperty("title").GetString().Should().Be(request.Title);
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task Error_Empty_Title(string culture)
+    {
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Title = string.Empty;
+
+        var result = await DoPost(requestUri: METHOD, request: request, token: _token, culture: culture);
+
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+
+        var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray();
+
+        var expectedMessage = ResourceErrorMessages.ResourceManager.GetString("TITLE_REQUIRED", new CultureInfo(culture));
+
+        errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(expectedMessage));
+    }
+}
