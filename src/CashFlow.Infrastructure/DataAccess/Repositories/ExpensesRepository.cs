d2a281c94d13e19447aedb7ff60f02c30e795ccd
Matheus Rocco <matheushrocco@gmail.com>
2024-11-18 15:48:06 -0500

    feat: return tag in expense

--- src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
+++ src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
@@ -1,6 +1,7 @@
 ﻿using CashFlow.Domain.Entities;
 using CashFlow.Domain.Repositories.Expenses;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace CashFlow.Infrastructure.DataAccess.Repositories;
 
@@ -31,14 +32,14 @@ internal class ExpensesRepository : IExpensesWriteOnlyRepository, IExpensesReadO
 
     async Task<Expense?> IExpensesReadOnlyRepository.GetById(User user, long id)
     {
-        return await _dbContext.Expenses
+        return await GetFullExpense()
             .AsNoTracking()
             .FirstOrDefaultAsync(expense => expense.Id == id && expense.UserId == user.Id);
     }
 
     async Task<Expense?> IExpensesUpdateOnlyRepository.GetById(User user, long id)
     {
-        return await _dbContext.Expenses
+        return await GetFullExpense()
             .FirstOrDefaultAsync(expense => expense.Id == id && expense.UserId == user.Id);
     }
 
@@ -61,5 +62,11 @@ internal class ExpensesRepository : IExpensesWriteOnlyRepository, IExpensesReadO
             .OrderBy(expense => expense.Date)
             .ThenBy(expense => expense.Title)
             .ToListAsync();
+    }
+
+    private IIncludableQueryable<Expense, ICollection<Tag>> GetFullExpense()
+    {
+        return _dbContext.Expenses
+            .Include(expense => expense.Tags);
     }
 }
